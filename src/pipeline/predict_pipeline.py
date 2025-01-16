c502ad266707754ce4e95256705f3001f530afc6
jugal1997 <89485766+jugal1997@users.noreply.github.com>
2025-01-16 15:44:34 -0500

    Debuged app.py file

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import pandas as pd
 from src.exception import CustomException
@@ -9,8 +10,8 @@ class PredictPipeline:
 
     def predict(self,features):
         try:
-            model_path='artifacts\model.pkl'
-            preprocessor_path='artifacts\preprocessor.pkl'
+            model_path=os.path.join("artifacts","model.pkl")
+            preprocessor_path=os.path.join('artifacts','proprocessor.pkl')
             model=load_object(file_path=model_path)
             preprocessor=load_object(file_path=preprocessor_path)
             data_scaled=preprocessor.transform(features)
@@ -22,7 +23,7 @@ class PredictPipeline:
 
 
 class CustomData:
-    def __init(self,
+    def __init__(self,
         gender: str,
         race_ethnicity: str,
         parental_level_of_education,
